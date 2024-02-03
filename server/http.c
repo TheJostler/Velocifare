7f8bfeb453810c749e2263277e9e5d3abb37c678
Josjuar Lister <65976463+TheJostler@users.noreply.github.com>
2024-02-03 23:57:44 +0000

    .

--- server/http.c
+++ server/http.c
@@ -41,7 +41,7 @@ int http_parse(char *packet) {
 
 int http_respond() {
     char *http1;
-    snprintf(http1, 14, "HTTP/%f %i\r\n", 1.1, http_status);
+    snprintf(http1, 14, "HTTP/%s %i\r\n", "1.1", http_status);
     put(http1);
     put("Host: siteinc.tego\r\n");
     put("\r\n");
