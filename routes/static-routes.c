fde9f7f329743617e1bd74a4bfe078a640ef2de4
Josjuar Lister <65976463+TheJostler@users.noreply.github.com>
2024-02-04 00:29:05 +0000

    .

--- routes/static-routes.c
+++ routes/static-routes.c
@@ -1,5 +1,5 @@
-#include "../../views/headers/views.h"
-#include "../../server/headers/http.h"
+#include "../views/headers/views.h"
+#include "../server/headers/http.h"
 
 /*This file will be generated at build
 time and will contain route definitions
