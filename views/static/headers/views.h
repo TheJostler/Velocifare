7dab29f59c56331a15fac5fdd71e9867170c1661
Josjuar Lister <65976463+TheJostler@users.noreply.github.com>
2024-02-07 21:56:59 +0000

    Update views.h

--- views/static/headers/views.h
+++ views/static/headers/views.h
@@ -1,11 +1,1 @@
 
-int render___app_immutable_chunks_entry__NMAPGFp_js();
-int render___app_immutable_chunks_index_DFGvIiBm_js();
-int render___app_immutable_chunks_scheduler_zMJaRgub_js();
-int render___app_immutable_entry_app_8Tk0J5hO_js();
-int render___app_immutable_entry_start_zIs2AfV__js();
-int render___app_immutable_nodes_0_AjdpMCkz_js();
-int render___app_immutable_nodes_1_V0jkrtux_js();
-int render___app_immutable_nodes_2_cD2tf3Wt_js();
-int render___app_env_js();
-int render__index_html();
