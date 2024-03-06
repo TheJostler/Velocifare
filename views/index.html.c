882ab028642ef9f8f7e8e15ec3e035aefd4ff127
Josjuar Lister <65976463+TheJostler@users.noreply.github.com>
2024-03-06 16:57:55 +0000

    Update index.html.c

--- views/index.html.c
+++ views/index.html.c
@@ -11,7 +11,7 @@ int render_index() {
 		"		",
 		"	</head>",
 		"	<body data-sveltekit-preload-data=\"hover\">",
-		"		<div>  <h1 data-svelte-h=\"svelte-15pgwlj\">Welcome to Site in C Feat. Svelte</h1> <p data-svelte-h=\"svelte-td5yc3\">This site is a working static Svelte app that is compiled by SiteInC into the webserver itself. The webapp and server are compiled directly into a single executable file.</p> <p data-svelte-h=\"svelte-1gcp1ig\">Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p> <p data-svelte-h=\"svelte-jl9sbz\">Visit <a href=\"https://kit.svelte.dev\">kit.svelte.dev</a> to read the documentation</p> ",
+		"		<div>  <h1 data-svelte-h=\"svelte-15pgwlj\">Welcome to Velocifare Feat. Svelte</h1> <p data-svelte-h=\"svelte-td5yc3\">This site is a working static Svelte app that is compiled by SiteInC into the webserver itself. The webapp and server are compiled directly into a single executable file.</p> <p data-svelte-h=\"svelte-1gcp1ig\">Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p> <p data-svelte-h=\"svelte-jl9sbz\">Visit <a href=\"https://kit.svelte.dev\">kit.svelte.dev</a> to read the documentation</p> ",
 		"			",
 		"		</div>",
 		"	</body>",
